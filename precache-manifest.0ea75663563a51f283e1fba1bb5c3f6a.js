a01b5bc0d20bda59af3575809851c3c86426c622
Sebastian Barcatt Mirande <sbarcatt@gmail.com>
2020-10-18 02:22:22 -0300

    Updates

---
+++ precache-manifest.0ea75663563a51f283e1fba1bb5c3f6a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed0a8431ae2706e0f1ea1e6d53c5f99",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3b08a1a0ac6bbcbd180b",
+    "url": "/portfolio/static/css/main.23a47dc6.chunk.css"
+  },
+  {
+    "revision": "4d3bc041b232a9a789ab",
+    "url": "/portfolio/static/js/2.99e71681.chunk.js"
+  },
+  {
+    "revision": "85913a71cdb5915b55da7a2780e5858f",
+    "url": "/portfolio/static/js/2.99e71681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b08a1a0ac6bbcbd180b",
+    "url": "/portfolio/static/js/main.eccb462e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "1e7568d33ed66a9c9f08d073c27f9adc",
+    "url": "/portfolio/static/media/photo.1e7568d3.jpeg"
+  },
+  {
+    "revision": "b0d642d45ece3bfbe27a240ed9bc60ce",
+    "url": "/portfolio/static/media/tensolite1.b0d642d4.png"
+  },
+  {
+    "revision": "1abb9b0a3e5ae84168b4b82d180af44b",
+    "url": "/portfolio/static/media/tensolite2.1abb9b0a.png"
+  },
+  {
+    "revision": "2b97b176bc7bfdced22e671de728e7a6",
+    "url": "/portfolio/static/media/tensolite3.2b97b176.png"
+  },
+  {
+    "revision": "a8b1d3356089e54c983346f0996e7240",
+    "url": "/portfolio/static/media/tensolite4.a8b1d335.png"
+  },
+  {
+    "revision": "2fbe5d075e068751dfc06156f19d2395",
+    "url": "/portfolio/static/media/tensolite5.2fbe5d07.png"
+  },
+  {
+    "revision": "f413d55f801164f4d2ace09d2b9808d6",
+    "url": "/portfolio/static/media/tensolite6.f413d55f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.fdafe65988586bdc0eb8af113739fa24.js"
+  "/portfolio/precache-manifest.0ea75663563a51f283e1fba1bb5c3f6a.js"
 );
 
 self.addEventListener('message', (event) => {
